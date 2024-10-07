765bd6bdc6804a25ad681155d5f1200c0925abd8
SEspy78 <baokhang212121@gmail.com>
2024-10-07 23:06:18 +0700

    Home page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' mt-10 text-center border-t-[1px] border-white'>
+      <p>This is footer</p>
+    </div>
+  )
+}
+
+export default Footer
